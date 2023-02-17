b0640d4000e04d91d5839dad5da8dc25786abc3d
Prodip Karmakar <prodipkar@gmail.com>
2023-02-18 01:40:10 +0530

    updated jenkins file

--- src/main/java/com/nilsoft/bookshop/controller/BookController.java
+++ src/main/java/com/nilsoft/bookshop/controller/BookController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/books")
 public class BookController {
 
     @GetMapping("/{bookId}")
@@ -23,5 +25,26 @@ public class BookController {
         book.setPrice("INR 990");
         book.setYearPublished("2017");
         return book;
+    }
+
+    @GetMapping("/")
+    public List<Book> getBooks(){
+        Book book = new Book();
+        book.setId("xd01245");
+        book.setAuthor("Joshua Bloch");
+        book.setName("Effective Java");
+        book.setVersion("3");
+        book.setPrice("INR 990");
+        book.setYearPublished("2017");
+
+        Book book1 = new Book();
+        book1.setId("PF01245");
+        book1.setAuthor("Narashima");
+        book1.setName("Java Data structure");
+        book1.setVersion("2");
+        book1.setPrice("INR 660");
+        book1.setYearPublished("2015");
+
+        return Arrays.asList(book,book1);
     }
 }
